281272cc5a5fd761fdd33a7bedb714668b09e0df
Logan Dhillon <dev@logandhillon.com>
2026-07-31 16:38:29 -0400

    feat: adopt Terminal for IO use

--- src/bdsh/shell.py
+++ src/bdsh/shell.py
@@ -1,22 +1,15 @@
 import os
 import subprocess
 import sys
-from contextlib import redirect_stdout
-from typing import TextIO
 
-from bdsh import NL, __version__, get_shell_path, SHELL_COPYRIGHT
+from bdsh import NL, get_shell_path, SHELL_COPYRIGHT
 from bdsh.command.commands import register_commands
-from bdsh.user import User
-from bdsh.user.session import SessionManager
+from bdsh.session import Session
 
 
 class Shell:
-    def __init__(self, stdout: TextIO | None, stdin: TextIO | None, user: User, **is_ssh: bool):
-        self.stdout = stdout
-        self.stdin = stdin
-        self.print = lambda s: self.stdout.write(s)
-        self.readchar = lambda: self.stdin.read(1)
-        self.is_ssh = is_ssh
+    def __init__(self, session: Session):
+        self.session = session
         self.path = get_shell_path()
 
         self.header = SHELL_COPYRIGHT
@@ -31,10 +24,8 @@ class Shell:
         self.env = os.environ.copy()
         self.env['PYTHONPATH'] = os.path.dirname(os.path.realpath(__file__))
 
-        self.session = SessionManager(user)
-
     def fatal(self, msg: str, exit_code: int = 129):
-        self.print(f"FATAL({exit_code}): {msg}{NL}")
+        self.session.io.println(f"FATAL({exit_code}): {msg}")
         exit(exit_code)
 
     def run_line(self, line: str):
@@ -49,19 +40,14 @@ class Shell:
             self.run_line(self.definitions[args[0]] + " " + ' '.join(args[1:]))
         elif args[0] in self.commands:
             try:
-                with redirect_stdout(self.stdout):
-                    self.commands[args[0]].execute(args)
+                self.commands[args[0]].execute(args)
             except Exception as e:
-                self.print(f"{args[0]}: {e}")
+                self.session.io.print(f"{args[0]}: {e}")
         elif os.path.exists(binary := get_shell_path("exec", args[0])):
-            if self.is_ssh:
-                self.print(f"{args[0]} is unsupported over SSH")
-                return
-
             subprocess.run([sys.executable, binary] + args[1:], stdout=self.stdout, stderr=subprocess.STDOUT,
                            stdin=self.stdin, text=True, env=self.env)
         else:
-            self.print(f"Invalid command: {args[0]}")
+            self.session.io.println(f"invalid command: {args[0]}")
 
     def cwd(self):
         return self.path
@@ -84,43 +70,40 @@ class Shell:
         if not self.session.is_logged_in():
             self.fatal("bdsh: attempted to instantiate shell without logging in", 131)
 
-        self.print(f"Welcome, {self.session.get_user().username}!{NL}")
+        self.session.io.println(f"Welcome, {self.session.get_user().username}!")
         self.chdir(self.session.userhome)
 
         # start processing commands
-        self.print(self.get_prompt())
+        self.session.io.print(self.get_prompt())
         buffer = []
 
         while True:
             try:
-                char = self.readchar()
-
-                if self.is_ssh:
-                    self.print(char)
+                char = self.session.io.read(1)
 
                 if char in {'\n', '\r'}:
                     if char == '\r':
-                        self.print('\n')
+                        self.session.io.print('\n')
                     self.run_line(''.join(buffer))
                     buffer.clear()
-                    self.print(self.get_prompt())
+                    self.session.io.print(self.get_prompt())
                 elif char == '\x03':  # ^C
                     buffer.clear()
-                    self.print(self.get_prompt())
+                    self.session.io.print(self.get_prompt())
                 elif char == '\x7f':  # backspace
                     if len(buffer) <= 0:
                         continue
-                    self.print('\x08 \x08')
+                    self.session.io.print('\x08 \x08')
                     buffer.pop()
                 else:
                     buffer.append(char)
 
             except KeyboardInterrupt:
                 buffer.clear()
-                self.print(self.get_prompt())
+                self.session.io.print(self.get_prompt())
                 continue
 
             except Exception as e:
                 buffer.clear()
-                self.print(f"bdsh: unhandled exception: {e}{NL}{self.get_prompt()}")
+                self.session.io.print(f"bdsh: unhandled exception: {e}{NL}{self.get_prompt()}")
                 continue
