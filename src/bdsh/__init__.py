0c45395b0ae757f5cf5ef1ea19692c788c80e7ce
Logan Dhillon <dev@logandhillon.com>
2026-08-02 01:44:21 -0400

    fix(fs): circular dependency on ROOT_DIR

--- src/bdsh/__init__.py
+++ src/bdsh/__init__.py
@@ -4,15 +4,16 @@ from typing import Final
 from .__version__ import __version__
 
 NL = '\r\n'
+_ROOT_DIR = Path("bdsh").resolve()
 SHELL_COPYRIGHT = f"BadOS Dynamic Shell (v{__version__}){NL}(c) Bad Technologies. All rights reserved.{NL}"
 
 
 def get_shell_path(*paths: str):
-    return OSPaths.ROOT.joinpath(*paths)
+    return _ROOT_DIR.joinpath(*paths)
 
 
 class OSPaths:
-    ROOT: Final[Path] = Path("bdsh").resolve()
+    ROOT: Final[Path] = _ROOT_DIR
 
     APPLICATIONS: Final[Path] = get_shell_path("app")
     CONFIGS: Final[Path] = get_shell_path("cfg")
