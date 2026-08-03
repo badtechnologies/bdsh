b2344f50c165900780e3a0a5e9d7748cdf6b9bdf
Logan Dhillon <dev@logandhillon.com>
2026-08-03 16:53:53 -0400

    feat(badproc): discover all services for execution

--- src/bdsh/command/badproc.py
+++ src/bdsh/command/badproc.py
@@ -1,7 +1,9 @@
 import argparse
+import importlib
+import pkgutil
 from typing import List
 
-from bdsh import SHELL_COPYRIGHT
+import bdsh
 from bdsh.command import Command
 from bdsh.service import SERVICES, Service
 
@@ -13,15 +15,21 @@ def _get_service(name: str) -> Service:
     return SERVICES[name]()
 
 
+def _discover_services():
+    for module in pkgutil.walk_packages(bdsh.service.__path__, prefix=bdsh.service.__name__ + ".", ):
+        importlib.import_module(module.name)
+
+
 class BadProcessManagerCommand(Command):
     def __init__(self, session):
         super().__init__(session)
+        _discover_services()
 
         self.parser = argparse.ArgumentParser(description="BadProc Service Manager", color=False)
         self.parser.add_argument('service', type=str, help='name of service to launch')
 
     def execute(self, args: List[str]):
-        self.session.io.println("BadProc Service Manager, " + SHELL_COPYRIGHT)
+        self.session.io.println("BadProc Service Manager, " + bdsh.SHELL_COPYRIGHT)
 
         args = self.parser.parse_args(args[1:])
         service = _get_service(args.service)
